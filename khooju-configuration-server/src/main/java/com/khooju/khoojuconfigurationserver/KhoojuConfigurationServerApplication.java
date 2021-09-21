cf0d8212c8abf321602dadc81371492d6b09107a
mukesh <mukesh.kumar@vecna.com>
2021-09-21 22:22:04 +0530

    added configuration server for centralised location

---
+++ khooju-configuration-server/src/main/java/com/khooju/khoojuconfigurationserver/KhoojuConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.khooju.khoojuconfigurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhoojuConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhoojuConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ khooju-configuration-server/src/test/java/com/khooju/khoojuconfigurationserver/KhoojuConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khooju.khoojuconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhoojuConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
